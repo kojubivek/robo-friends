b49cca452dc97b6d21511325ba15b69b400973b3
koju <kojubivek@gmail.com>
2020-07-04 01:22:58 +1000

    react project

---
+++ src/Robots.js
@@ -0,0 +1,40 @@
+export const robots =[
+{
+	id:1,
+	name : 'Nischal Maharjan',
+	Username: 'Nischal',
+	email: 'nischal@gmail.com'
+},
+{
+	id:2,
+	name : 'Bivek Koju',
+	Username: 'Bivek',
+	email: 'Bivek@gmail.com'
+},
+{
+	id:3,
+	name : 'Roman 100',
+	Username: 'Roman',
+	email: 'Roman@gmail.com'
+},
+{
+	id:5,
+	name : 'Aabhash Shrestha',
+	Username: 'Aabhash',
+	email: 'Aabhash@gmail.com'
+},
+{
+	id:4,
+	name : 'Abiral Karki',
+	Username: 'Abiral',
+	email: 'chumchum@gmail.com'
+},
+{
+	id:6,
+	name : 'Prabish Shrestha',
+	Username: 'Prabish',
+	email: 'sususus@gmail.com'
+},
+
+
+]
